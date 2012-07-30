915e281511d794429d94ec9a02c5feceaf796338
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2012-07-30 23:03:39 +0200

    Added a hardcoded testfile

--- c64iec.ino
+++ c64iec.ino
@@ -380,9 +380,27 @@ void send_dir() {
   put_byte(0, 1);
 }
 
+uchar testfile[34] = { 
+  0x01, 0x08, 0x16, 0x08, 0x0a, 0x00, 0x99, 0x20, 
+  0x22, 0x48, 0x45, 0x4c, 0x4c, 0x4f, 0x20, 0x57, 
+  0x4f, 0x52, 0x4c, 0x44, 0x21, 0x22, 0x00, 0x1f, 
+  0x08, 0x14, 0x00, 0x89, 0x20, 0x31, 0x30, 0x00, 
+  0x00, 0x00 };
+
+void send_testfile() {
+  int eoi;
+  for(int i=0;i<sizeof(testfile);i++) {
+    eoi = 0;
+    if(i == (sizeof(testfile)-1)) eoi = 1;
+    put_byte(testfile[i], eoi);
+  }
+}
+
 void handle_talk(uchar sec_addr) {
   if(strlen((const char *)iobuf) == 1 && iobuf[0] == '$') {
     send_dir();
+  } else if(strlen((const char *)iobuf) == 8 && !strcmp((const char *)iobuf, "TESTFILE")) {
+    send_testfile();
   }
 }
 
