102e092390359731aceca4cf64fe430dc82a81b4
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2012-09-02 12:58:16 +0200

    Add possibility to provide paths on commandline

--- tools/c64iecd.rb
+++ tools/c64iecd.rb
@@ -9,5 +9,30 @@ require 'device'
 require 'tftp'
 
 if __FILE__ == $0
-  iec = TFTP.run()
+  options = {}
+
+  ARGV.each do |argv|
+    if(argv[/^(\d+):(.*)/])
+      id = $1.to_i
+      path = Pathname.new($2)
+      next if !path.directory? && path.extname.downcase != ".d64"
+      options[:devices] ||= []
+      options[:devices] <<= {
+        :id => id,
+        :path => path.to_s,
+        :type => path.directory? ? :dir : :d64
+      }
+    else
+      id = 8
+      path = Pathname.new(argv)
+      next if !path.directory? && path.extname.downcase != ".d64"
+      options[:devices] = [{
+        :id => id,
+        :path => path.to_s,
+        :type => path.directory? ? :dir : :d64
+      }]
+    end
+  end
+
+  iec = TFTP.run(options)
 end
